cbaf0e6c3e9b636f9dce0753eb3cc1b96139ca58
Chandler Gonzales <jcgsville@gmail.com>
2020-04-07 14:52:06 -0700

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import schema1 from './schema1.json';
 import schema2 from './schema2.json';
 
-const example1: [ { foo: number; bar: number }, { baz: number; } ] = schema1;
+const example1: [ { foo: number; bar: number }, { baz: string; } ] = schema1;
 const example2: [ number, string ] = schema2;
 
 const schema3: [ { foo: number; bar: number }, { baz: string; } ] = [
