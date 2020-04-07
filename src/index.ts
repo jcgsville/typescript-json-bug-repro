00910494396c50ce3541fd2d80c7a81982e71865
Chandler Gonzales <jcgsville@gmail.com>
2020-04-07 12:24:52 -0700

    show the bug repro

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import schema1 from './schema1.json';
+import schema2 from './schema2.json';
+
+const example1: [ { foo: number; bar: number }, { baz: number; } ] = schema1;
+const example2: [ number, string ] = schema2;
